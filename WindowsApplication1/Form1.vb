d90d23e481fe993fb4ab0a0b5a7dab2c885df339
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-10-10 11:45:07 -0500

    Another commit.

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -1,4 +1,8 @@
-﻿Imports System
+﻿'Joshua Pickenpaugh
+'October 10th, 2016
+'Self-modified DB app (made table and app)
+
+Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Data
@@ -12,7 +16,11 @@ Imports System.Windows.Forms
 Public Class Form1
     Private Sub btnClick_Click(sender As Object, e As EventArgs) Handles btnClick.Click
 
+        '"fname" is the table, so using "first_name" is needed since that is the 
+        'litteral column name in the table I created. ___IT WORKED!___:
+
         Dim cmdString As String = "SELECT fname.first_name FROM TEST_DBEntities.persons as fname"
+        'This was changed to "name=TEST_DBEntities":
         Dim Conn = New EntityConnection("name=TEST_DBEntities")
         Conn.Open()
         Dim cmd = Conn.CreateCommand()
@@ -23,6 +31,7 @@ Public Class Form1
         lstBoxPersons.Items.Clear()
 
         While (rd.Read())
+            'This was changed to "first_name":
             lstBoxPersons.Items.Add(rd("first_name"))
         End While
 
