4b65a2ef70c45be76750cac41e29e83c09f520e5
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-10-10 12:35:00 -0500

    Final Commit

--- WindowsApplication1/Form1.Designer.vb
+++ WindowsApplication1/Form1.Designer.vb
@@ -22,40 +22,93 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.lstBoxPersons = New System.Windows.Forms.ListBox()
+        Me.lstBoxFirstNames = New System.Windows.Forms.ListBox()
         Me.btnClick = New System.Windows.Forms.Button()
+        Me.lstBoxLastNames = New System.Windows.Forms.ListBox()
+        Me.btnClick2 = New System.Windows.Forms.Button()
+        Me.lstBoxEmail = New System.Windows.Forms.ListBox()
+        Me.btnClick3 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'lstBoxPersons
+        'lstBoxFirstNames
         '
-        Me.lstBoxPersons.FormattingEnabled = True
-        Me.lstBoxPersons.Location = New System.Drawing.Point(13, 13)
-        Me.lstBoxPersons.Name = "lstBoxPersons"
-        Me.lstBoxPersons.Size = New System.Drawing.Size(120, 95)
-        Me.lstBoxPersons.TabIndex = 0
+        Me.lstBoxFirstNames.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstBoxFirstNames.FormattingEnabled = True
+        Me.lstBoxFirstNames.ItemHeight = 16
+        Me.lstBoxFirstNames.Location = New System.Drawing.Point(13, 13)
+        Me.lstBoxFirstNames.Name = "lstBoxFirstNames"
+        Me.lstBoxFirstNames.Size = New System.Drawing.Size(265, 84)
+        Me.lstBoxFirstNames.TabIndex = 0
         '
         'btnClick
         '
-        Me.btnClick.Location = New System.Drawing.Point(13, 115)
+        Me.btnClick.Location = New System.Drawing.Point(13, 103)
         Me.btnClick.Name = "btnClick"
-        Me.btnClick.Size = New System.Drawing.Size(120, 23)
+        Me.btnClick.Size = New System.Drawing.Size(265, 39)
         Me.btnClick.TabIndex = 1
-        Me.btnClick.Text = "Show Persons"
+        Me.btnClick.Text = "Show First Names"
         Me.btnClick.UseVisualStyleBackColor = True
+        '
+        'lstBoxLastNames
+        '
+        Me.lstBoxLastNames.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstBoxLastNames.FormattingEnabled = True
+        Me.lstBoxLastNames.ItemHeight = 16
+        Me.lstBoxLastNames.Location = New System.Drawing.Point(13, 149)
+        Me.lstBoxLastNames.Name = "lstBoxLastNames"
+        Me.lstBoxLastNames.Size = New System.Drawing.Size(265, 84)
+        Me.lstBoxLastNames.TabIndex = 2
+        '
+        'btnClick2
+        '
+        Me.btnClick2.Location = New System.Drawing.Point(13, 238)
+        Me.btnClick2.Name = "btnClick2"
+        Me.btnClick2.Size = New System.Drawing.Size(265, 39)
+        Me.btnClick2.TabIndex = 3
+        Me.btnClick2.Text = "Sbow Last Names"
+        Me.btnClick2.UseVisualStyleBackColor = True
+        '
+        'lstBoxEmail
+        '
+        Me.lstBoxEmail.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstBoxEmail.FormattingEnabled = True
+        Me.lstBoxEmail.ItemHeight = 16
+        Me.lstBoxEmail.Location = New System.Drawing.Point(13, 284)
+        Me.lstBoxEmail.Name = "lstBoxEmail"
+        Me.lstBoxEmail.Size = New System.Drawing.Size(265, 84)
+        Me.lstBoxEmail.TabIndex = 4
+        '
+        'btnClick3
+        '
+        Me.btnClick3.Location = New System.Drawing.Point(13, 372)
+        Me.btnClick3.Name = "btnClick3"
+        Me.btnClick3.Size = New System.Drawing.Size(265, 39)
+        Me.btnClick3.TabIndex = 5
+        Me.btnClick3.Text = "Show Email"
+        Me.btnClick3.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(147, 156)
+        Me.ClientSize = New System.Drawing.Size(290, 429)
+        Me.Controls.Add(Me.btnClick3)
+        Me.Controls.Add(Me.lstBoxEmail)
+        Me.Controls.Add(Me.btnClick2)
+        Me.Controls.Add(Me.lstBoxLastNames)
         Me.Controls.Add(Me.btnClick)
-        Me.Controls.Add(Me.lstBoxPersons)
+        Me.Controls.Add(Me.lstBoxFirstNames)
         Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form1"
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents lstBoxPersons As ListBox
+    Friend WithEvents lstBoxFirstNames As ListBox
     Friend WithEvents btnClick As Button
+    Friend WithEvents lstBoxLastNames As ListBox
+    Friend WithEvents btnClick2 As Button
+    Friend WithEvents lstBoxEmail As ListBox
+    Friend WithEvents btnClick3 As Button
 End Class
--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -28,14 +28,62 @@ Public Class Form1
         Dim rd As EntityDataReader
         rd = cmd.ExecuteReader(CommandBehavior.SequentialAccess)
         'Clears the list box before being filled:
-        lstBoxPersons.Items.Clear()
+        lstBoxFirstNames.Items.Clear()
 
         While (rd.Read())
             'This was changed to "first_name":
-            lstBoxPersons.Items.Add(rd("first_name"))
+            lstBoxFirstNames.Items.Add(rd("first_name"))
         End While
 
         Conn.Close()
+
+    End Sub
+
+    Private Sub btnClick2_Click(sender As Object, e As EventArgs) Handles btnClick2.Click
+
+        '"fname" is the table, so using "first_name" is needed since that is the 
+        'litteral column name in the table I created. ___IT WORKED!___:
+
+        Dim cmdString As String = "SELECT fname.last_name FROM TEST_DBEntities.persons as fname"
+        'This was changed to "name=TEST_DBEntities":
+        Dim Conn = New EntityConnection("name=TEST_DBEntities")
+        Conn.Open()
+        Dim cmd = Conn.CreateCommand()
+        cmd.CommandText = cmdString
+        Dim rd As EntityDataReader
+        rd = cmd.ExecuteReader(CommandBehavior.SequentialAccess)
+        'Clears the list box before being filled:
+        lstBoxLastNames.Items.Clear()
+
+        While (rd.Read())
+            'This was changed to "first_name":
+            lstBoxLastNames.Items.Add(rd("last_name"))
+        End While
+
+        Conn.Close()
+
+    End Sub
+
+    Private Sub btnClick3_Click(sender As Object, e As EventArgs) Handles btnClick3.Click
+
+        Dim cmdString As String = "SELECT fname.email FROM TEST_DBEntities.persons as fname"
+        'This was changed to "name=TEST_DBEntities":
+        Dim Conn = New EntityConnection("name=TEST_DBEntities")
+        Conn.Open()
+        Dim cmd = Conn.CreateCommand()
+        cmd.CommandText = cmdString
+        Dim rd As EntityDataReader
+        rd = cmd.ExecuteReader(CommandBehavior.SequentialAccess)
+        'Clears the list box before being filled:
+        lstBoxEmail.Items.Clear()
+
+        While (rd.Read())
+            'This was changed to "first_name":
+            lstBoxEmail.Items.Add(rd("email"))
+        End While
+
+        Conn.Close()
+
 
     End Sub
 End Class
